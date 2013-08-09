e4ba7bac4c1afab7be1947ec32ced8331abd876d
Jarrod Carlson <jarrod.carlson@gmail.com>
2013-08-08 21:50:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hbs::Application.config.secret_token = '3dedd07a1637dfbdaae4f8b305d845f678d0be80f66de34ee7e4d987370742b54adee75ac8ca5637bebc108100bb66d77f137c0f9e9f7622ca186104a9555d7a'
